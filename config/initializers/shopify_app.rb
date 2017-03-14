77c52f1b6c7c88828aa8de29c35597f88c86e325
AndyWang <andywang1116@gmail.com>
2017-03-14 16:16:25 +0800

    create shopify app

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,6 @@
+ShopifyApp.configure do |config|
+  config.api_key = "44840fbaa5a5f7490a2677c6c889cee4"
+  config.secret = "998df49b50f5b49b6bb24f8b7bde7ed3"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+end
