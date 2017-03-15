940b6784c7778a369b9ca2e66f1c4ef95eb0fb66
AndyWang <andywang1116@gmail.com>
2017-03-15 11:11:38 +0800

    add settings in order to install app to the shop of shopify

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :shopify,
     ShopifyApp.configuration.api_key,
     ShopifyApp.configuration.secret,
+    callback_url: "https://07a43f01.ngrok.io/auth/shopify/callback",
     scope: ShopifyApp.configuration.scope
 end
